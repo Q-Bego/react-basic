6f8f5d75d13cf759cddce94cd3a4b463abdb686d
Q-Bego <3213019652@qq.com>
2026-05-24 23:36:03 +0800

    挂载、更新和卸载的钩子函数

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // 1.兄弟关系
 // 2.后代关系
 import { createRoot } from 'react-dom/client'
-import App from './App11'
+import App from './App12'
 const root = createRoot(document.querySelector('#root'))
 
 root.render(<App></App>)
