809f9580e86ed70115cb9a750ef15428827fbf83
Q-Bego <3213019652@qq.com>
2026-05-28 21:12:47 +0800

    feat: reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-//非父子之间的通信
-// 1.兄弟关系
-// 2.后代关系
 import { createRoot } from 'react-dom/client'
-import App from './App15'
+import App from './App'
 const root = createRoot(document.querySelector('#root'))
 
 root.render(<App></App>)
